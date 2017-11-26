1f40758dfd6f37468a9bfc277f489babd3b8de17
Larry Custodio <custodioly@gmail.com>
2017-11-26 14:07:22 -0800

    added webpack config

---
+++ src/js/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+render(
+    <div className='map-bot-container'>
+        Loading...
+    </div>
+    ,
+    document.getElementById('app')
+);
